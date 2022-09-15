e314cae3a23a1ac4db9ba49e8c156ac60b99ddb0
Adejinmi <106541185+Adejinmi@users.noreply.github.com>
2022-09-15 16:28:57 +0100

    another

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,8 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void positive_or_negative(int);
-int largest_number(int, int, int);
-void print_remaning_days(int, int, int);
-int convert_day(int, int);
+void positive_or_negative(int i);
 
-#endif
+#endif /* MAIN_H */
