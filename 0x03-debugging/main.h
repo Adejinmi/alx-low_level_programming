f7db24d44da644ef7e7c89d8ceff16ccf8ba702f
Samuel <adejinmisamuel1@gmail.com>
2022-09-15 07:57:18 -0700

    another one

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,11 +2,7 @@
 
 #define MAIN_H
 
-
-
 #include <stdio.h>
-
-
 
 void positive_or_negative(int i);
 
@@ -16,8 +12,5 @@ void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
 
-
-
-#endif /* MAIN_H */
-
+#endif
 
